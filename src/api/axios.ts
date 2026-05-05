e60cc03f1693d20d0c04b17b4ff523ca43014d0b
Shabin Nicholas <163176715+ShabinNicholas@users.noreply.github.com>
2026-05-05 14:47:18 +0530

    final

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "web-production-78f7b.up.railway.app",
+  // baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000',
+  baseURL: import.meta.env.VITE_API_URL || 'https://web-production-31cae5.up.railway.app/',
 });
 
 api.interceptors.request.use((config) => {
